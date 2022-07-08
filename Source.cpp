610c85e672b0182c2e4b13622573e9a5dd6f37cc
ajaisaikumar <108928494+ajaisaikumar@users.noreply.github.com>
2022-07-08 18:19:52 +0530

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -9,23 +9,45 @@ void listallprocessname(DWORD processid)
 	HANDLE hprocess;
 	BOOL benumprocessmodule=FALSE;
 	HMODULE hmod;
+	PROCESS_MEMORY_COUNTERS pmc;
 	DWORD bytereq = 0;
-	wchar_t chprocessname[MAX_PATH] = L"unkonwn";
-	wchar_t chprocesspath[MAX_PATH] = L"unkonwn";
-	hprocess = OpenProcess(PROCESS_QUERY_INFORMATION | PROCESS_VM_READ, FALSE, processid);
+	MODULEINFO info={};
+	DWORD bp = sizeof(info);
+	wchar_t processname[MAX_PATH] = L"unkonwn";
+	wchar_t processpath[MAX_PATH] = L"unkonwn";
+	hprocess = OpenProcess(PROCESS_QUERY_INFORMATION | PROCESS_VM_READ|PROCESS_QUERY_LIMITED_INFORMATION, FALSE, processid);
+	if (NULL == hprocess)
+	{
+		return;
+	}
+	
 	if (NULL != hprocess)
 	{
 		benumprocessmodule = EnumProcessModulesEx(hprocess, &hmod, sizeof(hmod), &bytereq, LIST_MODULES_ALL);
 		if (benumprocessmodule == TRUE)
 		{
-			GetModuleBaseName(hprocess, hmod, chprocessname, sizeof(chprocessname) / sizeof(wchar_t));
-			GetModuleFileNameEx(hprocess, hmod, chprocesspath, sizeof(chprocesspath) / sizeof(wchar_t));
+			GetModuleBaseName(hprocess, hmod, processname, sizeof(processname) / sizeof(wchar_t));
+			GetModuleFileNameEx(hprocess, hmod, processpath, sizeof(processpath) / sizeof(wchar_t));
+			GetModuleInformation(hprocess, hmod, &info, bp);
+			
 		}
+	
 	}
-
-	wcout << "modulebasename:" << chprocessname << endl;
-	wcout << "process path:" << chprocesspath << endl;
+	
+	wcout << "modulebasename:" << processname << endl;
+	wcout << "process path:" << processpath << endl;
+	wcout << "load address of the module:" << info.lpBaseOfDll << endl;
+	wcout << "entry point of the module:" << info.EntryPoint << endl;
+	
+    if (GetProcessMemoryInfo(hprocess, &pmc, sizeof(pmc)))
+	{
+		
+		wcout << "pagefileusage:" << pmc.PagefileUsage << endl;
+		wcout << "pagefaultcount:" << pmc.PageFaultCount << endl;
+	}
+	
 	wcout << "process id:" << processid << endl;
+	wcout << "===================================================================================" << endl;
 	CloseHandle(hprocess);
 }
 
@@ -35,6 +57,7 @@ int main()
 	BOOL benumprocess = FALSE;
 	DWORD bytesreq = 0;
 	DWORD totalprocess = 0;
+	
 	benumprocess = EnumProcesses(process1, sizeof(process1), &bytesreq );
 
 	if (benumprocess == FALSE)
@@ -42,7 +65,10 @@ int main()
 		wcout << "failed" << endl;
 	}
 	totalprocess = bytesreq/ sizeof(DWORD);
+	
+	
 	wcout << "total number of processes:" << totalprocess<<endl;
+	wcout << "============================================================================" << endl;
 	for (size_t i = 0; i < totalprocess; i++)
 	{
 		listallprocessname(process1[i]);
